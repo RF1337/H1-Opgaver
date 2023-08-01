21951fe35945b5145133a8bfe2f7c9815b573aea
RF1337 <112627787+RF1337@users.noreply.github.com>
2023-08-01 08:38:56 +0200

    Add files via upload

---
+++ FordelingsHatten/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FordelingsHatten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortingHat sortingHat = new SortingHat();
+            Console.WriteLine(sortingHat.RandomizeHat());
+        }
+    }
+}
---
+++ FordelingsHatten/SortingHat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FordelingsHatten
+{
+    internal class SortingHat
+    {
+        public enum Hats
+        {
+            Gryffindor,
+            Hufflepuff,
+            Ravenclaw,
+            Slytherin
+        }
+
+        private Random random = new Random();
+
+        public Enum RandomizeHat()
+        {
+            return (Hats)random.Next(1, 4);
+        }
+    }
+}
