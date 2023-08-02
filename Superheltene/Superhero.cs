5debff380c2a9a0695c67c7697f341520a51a052
RF1337 <112627787+RF1337@users.noreply.github.com>
2023-08-02 08:04:13 +0200

    Add files via upload

---
+++ Superheltene/Superhero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheltene
+{
+    internal class Superhero
+    {
+        public string Identity = "";
+        public string Abilities = "";
+        public string Outfit = "";
+        public string MoralCode = "";
+        public string Enemies = "";
+        public string Fear = "";
+
+        public Superhero(string identity, string abilities, string outfit, string moralCode, string enemies, string fear)
+        {
+            Identity = identity;
+            Abilities = abilities;
+            Outfit = outfit;
+            MoralCode = moralCode;
+            Enemies = enemies;
+            Fear = fear;
+        }
+    }
+}
